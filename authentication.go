f8aeca4a70992a2c11aa9ff42111f50018d89368
colasgrenier1 <nicolas.grenier2@mail.mcgill.ca>
2019-03-25 12:18:43 -0400

    Update authentication.go

--- authentication.go
+++ authentication.go
@@ -1,1 +1,27 @@
 
+import (
+	"encoding/csv"
+	"encoding/base64"
+	"time"
+)
+
+type Session struct {
+	id			integer
+	active		boolean
+	username	string
+	address		string //ip address, etc.
+	hash		string//cookie value
+	last		Time
+}
+
+//Gets the URL
+fn GetURLEncodedSessionID(id integer) {
+}
+
+//Manages sessions
+type SessionManager {
+	csvFile		csv.Writer
+	sessions	[]Session
+	timeout		integer //number of seconds between each request until termination of account.
+}
+
