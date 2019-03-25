7a04e0c8a95c57f79bd381811509e3044d446b8b
colasgrenier1 <nicolas.grenier2@mail.mcgill.ca>
2019-03-25 13:15:56 -0400

    Update errors.go

--- errors.go
+++ errors.go
@@ -1,1 +1,18 @@
+typedef ErrorNumber int
+
+typedef Error struct {
+	number	ErrorNumber
+	text	string
+}
+
+
+
+func (e *Error) Error() string {
+	return fmt.Sprintf("E%06d %s.", e.number, e.text)
+}
+
+func UnknownFormatterError (formatter string) Error {
+	return &Error{
+}
+
 
